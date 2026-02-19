7234d5ceaacf78312450fdbb2afceb5bef025742
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-02-19 14:15:51 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app= express();
+const PORT= 3005;
+
+//Set the view engine
+app.set("view engine",'ejs');
+
+app.use(express.static('public'))
+
+app.get('/',(req, res)=>{
+    let user={
+        name: "Seadrah",
+        color: "Yellow",
+        food: "Sushi",
+        grade:3.6,
+        hobbies: ['Yoga', 'Poetry', 'Travel'],
+        games: ['Crusader Kings 3', 'Frostpunk', 'Stellaris', "Shadow of the Colossus"]
+    }
+    res.render("home", { user });
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost:${PORT}`)
+})
